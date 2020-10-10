b3f114c44702ee72e752f52f815fe5f10858d8c9
jlbuild <juliabuildbot@gmail.com>
2020-10-10 01:33:09 +0000

    libvorbis_jll build 1.3.6+6

--- src/wrappers/armv7l-linux-gnueabihf.jl
+++ src/wrappers/armv7l-linux-gnueabihf.jl
@@ -2,94 +2,29 @@
 export libvorbis, libvorbisenc, libvorbisfile
 
 using Ogg_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libvorbis`
-const libvorbis_splitpath = ["lib", "libvorbis.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libvorbis_path = ""
-
-# libvorbis-specific global declaration
-# This will be filled out by __init__()
-libvorbis_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libvorbis = "libvorbis.so.0"
-
-
-# Relative path to `libvorbisenc`
-const libvorbisenc_splitpath = ["lib", "libvorbisenc.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libvorbisenc_path = ""
-
-# libvorbisenc-specific global declaration
-# This will be filled out by __init__()
-libvorbisenc_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libvorbisenc = "libvorbisenc.so.2"
-
-
-# Relative path to `libvorbisfile`
-const libvorbisfile_splitpath = ["lib", "libvorbisfile.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libvorbisfile_path = ""
-
-# libvorbisfile-specific global declaration
-# This will be filled out by __init__()
-libvorbisfile_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libvorbisfile = "libvorbisfile.so.3"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("libvorbis")
+JLLWrappers.@declare_library_product(libvorbis, "libvorbis.so.0")
+JLLWrappers.@declare_library_product(libvorbisenc, "libvorbisenc.so.2")
+JLLWrappers.@declare_library_product(libvorbisfile, "libvorbisfile.so.3")
 function __init__()
-    global artifact_dir = abspath(artifact"libvorbis")
+    JLLWrappers.@generate_init_header(Ogg_jll)
+    JLLWrappers.@init_library_product(
+        libvorbis,
+        "lib/libvorbis.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Ogg_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Ogg_jll.LIBPATH_list,))
+    JLLWrappers.@init_library_product(
+        libvorbisenc,
+        "lib/libvorbisenc.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global libvorbis_path = normpath(joinpath(artifact_dir, libvorbis_splitpath...))
+    JLLWrappers.@init_library_product(
+        libvorbisfile,
+        "lib/libvorbisfile.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libvorbis_handle = dlopen(libvorbis_path)
-    push!(LIBPATH_list, dirname(libvorbis_path))
-
-    global libvorbisenc_path = normpath(joinpath(artifact_dir, libvorbisenc_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libvorbisenc_handle = dlopen(libvorbisenc_path)
-    push!(LIBPATH_list, dirname(libvorbisenc_path))
-
-    global libvorbisfile_path = normpath(joinpath(artifact_dir, libvorbisfile_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libvorbisfile_handle = dlopen(libvorbisfile_path)
-    push!(LIBPATH_list, dirname(libvorbisfile_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
--- src/wrappers/i686-w64-mingw32.jl
+++ src/wrappers/i686-w64-mingw32.jl
@@ -2,94 +2,29 @@
 export libvorbis, libvorbisenc, libvorbisfile
 
 using Ogg_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-LIBPATH_default = ""
-
-# Relative path to `libvorbis`
-const libvorbis_splitpath = ["bin", "libvorbis-0.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libvorbis_path = ""
-
-# libvorbis-specific global declaration
-# This will be filled out by __init__()
-libvorbis_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libvorbis = "libvorbis-0.dll"
-
-
-# Relative path to `libvorbisenc`
-const libvorbisenc_splitpath = ["bin", "libvorbisenc-2.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libvorbisenc_path = ""
-
-# libvorbisenc-specific global declaration
-# This will be filled out by __init__()
-libvorbisenc_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libvorbisenc = "libvorbisenc-2.dll"
-
-
-# Relative path to `libvorbisfile`
-const libvorbisfile_splitpath = ["bin", "libvorbisfile-3.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libvorbisfile_path = ""
-
-# libvorbisfile-specific global declaration
-# This will be filled out by __init__()
-libvorbisfile_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libvorbisfile = "libvorbisfile-3.dll"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("libvorbis")
+JLLWrappers.@declare_library_product(libvorbis, "libvorbis-0.dll")
+JLLWrappers.@declare_library_product(libvorbisenc, "libvorbisenc-2.dll")
+JLLWrappers.@declare_library_product(libvorbisfile, "libvorbisfile-3.dll")
 function __init__()
-    global artifact_dir = abspath(artifact"libvorbis")
+    JLLWrappers.@generate_init_header(Ogg_jll)
+    JLLWrappers.@init_library_product(
+        libvorbis,
+        "bin/libvorbis-0.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Ogg_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Ogg_jll.LIBPATH_list,))
+    JLLWrappers.@init_library_product(
+        libvorbisenc,
+        "bin/libvorbisenc-2.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global libvorbis_path = normpath(joinpath(artifact_dir, libvorbis_splitpath...))
+    JLLWrappers.@init_library_product(
+        libvorbisfile,
+        "bin/libvorbisfile-3.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libvorbis_handle = dlopen(libvorbis_path)
-    push!(LIBPATH_list, dirname(libvorbis_path))
-
-    global libvorbisenc_path = normpath(joinpath(artifact_dir, libvorbisenc_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libvorbisenc_handle = dlopen(libvorbisenc_path)
-    push!(LIBPATH_list, dirname(libvorbisenc_path))
-
-    global libvorbisfile_path = normpath(joinpath(artifact_dir, libvorbisfile_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libvorbisfile_handle = dlopen(libvorbisfile_path)
-    push!(LIBPATH_list, dirname(libvorbisfile_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(vcat(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ';')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
